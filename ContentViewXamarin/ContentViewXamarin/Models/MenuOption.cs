806ea4e16ea7daa031f03ad6b27bce295faa1b03
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-09-10 13:58:58 -0400

    Add project files.

---
+++ ContentViewXamarin/ContentViewXamarin/Models/MenuOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentViewXamarin.Models
+{
+    class MenuOption
+    {
+        public MenuOption(string title, int countPlaces, string picturePath)
+        {
+            Title = title;
+            CountPlaces = countPlaces;
+            PicturePath = picturePath;
+        }
+
+        public string Title { get; }
+        public int CountPlaces { get; }
+        public string PicturePath { get; }
+    }
+}
---
+++ ContentViewXamarin/ContentViewXamarin/ViewModels/HomeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentViewXamarin.Models;
+
+namespace ContentViewXamarin.ViewModels
+{
+    class HomeViewModel
+    {
+        public MenuOption BarAndHotelsMenuOption { get; }
+        public MenuOption FreeDinningMenuOption { get; }
+        public MenuOption CafesMenuOption { get; }
+        public MenuOption NearbyMenuOption { get; }
+        public MenuOption FastFoodsMenuOption { get; }
+        public MenuOption FeaturedFoodsMenuOption { get; }
+        public HomeViewModel()
+        {
+            BarAndHotelsMenuOption = new MenuOption("BarsIcon.png", 50, "Bars & Hotels");
+            FreeDinningMenuOption = new MenuOption("FreeDinningIcon.png", 25, "Free Dinning");
+            CafesMenuOption = new MenuOption("CafeIcon.png", 38, "Cafes");
+            NearbyMenuOption = new MenuOption("NearbyIcon.png", 24, "Nearby");
+            FastFoodsMenuOption = new MenuOption("FastFoodIcon.png", 19, "Fast Foods");
+            FeaturedFoodsMenuOption = new MenuOption("FeaturedFoodIcon.png", 41, "Featured Foods");
+        }
+    }
+}
